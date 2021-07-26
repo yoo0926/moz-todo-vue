504d1aa8140e813d3ae56440515c82144c228201
yoo0926 <wodydasktp@gmail.com>
2021-07-26 18:02:29 +0900

    prettier apply

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential",
-        "eslint-config-prettier",
-        "plugin:prettier/recommended"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "parser": "babel-eslint"
-    },
-    "plugins": [
-        "vue",
-        "html",
-        "standard"
-    ],
-    "rules": {
-        "indent":[
-            "error",
-            2
-        ],
-        "no-unused-vars": 1,
-        "no-use-before-define": 1,
-        "no-redeclare": 1,
-        "no-console":0,
-        "quotes": ["error", "single"]
-    }
+  env: {
+    browser: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'eslint-config-prettier',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    parser: 'babel-eslint'
+  },
+  plugins: ['vue', 'html', 'standard'],
+  rules: {
+    indent: ['error', 2],
+    'no-unused-vars': 1,
+    'no-use-before-define': 1,
+    'no-redeclare': 1,
+    'no-console': 0,
+    quotes: ['error', 'single']
+  }
 };

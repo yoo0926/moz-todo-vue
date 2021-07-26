7f9df3ac0238d4e7d355578087fb24ca4dc1381c
yoo0926 <wodydasktp@gmail.com>
2021-07-26 18:03:31 +0900

    styling todo vue

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,24 +1,35 @@
 <template>
-    <form @submit.prevent="onSubmit">
-        <label for="new-todo-input">
-            What needs to be done?
-        </label>
-        <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model.lazy.trim="label"/>
-        <button type="submit">Add</button>
-    </form>
+  <form @submit.prevent="onSubmit">
+    <label for="new-todo-input" class="label__lg">
+      What needs to be done?
+    </label>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+      class="input__lg"
+    />
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
+  </form>
 </template>
 
 <script>
-    export default {
-        methods: {
-            onSubmit() {
-                this.$emit("todo-added", this.label);
-            }
-        },
-        data() {
-            return {
-                label: ""
-            };
-        }
+export default {
+  methods: {
+    onSubmit() {
+      if (this.label === '') {
+        return;
+      }
+      this.$emit('todo-added', this.label);
+      this.label = '';
+    }
+  },
+  data() {
+    return {
+      label: ''
     };
+  }
+};
 </script>

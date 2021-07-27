a288f5e8d7ff14484ba103a1ef9795a6389eea47
yoo0926 <wodydasktp@gmail.com>
2021-07-27 18:06:17 +0900

    added edited, delete buttons

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
   <div id="app">
     <h1>My To-Do List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
+    <h2 id="list-summary">{{ listSummary }}</h2>
     <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item
           :label="item.label"
           :done="item.done"
           :id="item.id"
+          @checkbox-changed="updateDoneStatus(item.id)"
+          @item-deleted="deletedToDo(item.id)"
+          @item-edited="editToDo(item.id, $event)"
         ></to-do-item>
       </li>
     </ul>
@@ -46,6 +50,25 @@ export default {
         label: toDoLabel,
         done: false
       });
+    },
+    updateDoneStatus(toDoId) {
+      const toDoToUpdate = this.ToDoItems.find(item => item.id === toDoId);
+      toDoToUpdate.done = !toDoToUpdate.done;
+    },
+    deleteToDo(toDoId) {
+      const itemIndex = this.ToDoItems.findIndex(item => item.id === toDoId);
+      this.ToDoItems.splice(itemIndex, 1);
+    },
+    editToDo(toDoId, newLabel) {
+      const toDoToEdit = this.ToDoItems.find(item => item.id === toDoId);
+      toDoToEdit.label = newLabel;
+    }
+  },
+  computed: {
+    listSummary() {
+      const numberFinishedItems = this.ToDoItems.filter(item => item.done)
+        .length;
+      return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`;
     }
   }
 };
